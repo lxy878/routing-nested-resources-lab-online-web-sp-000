7782806c497e8f1dd505f8d98d8045f0facd83ef
Xiaoyi Liu <lxy878@gmail.com>
2020-07-05 16:14:48 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,7 @@ class SongsController < ApplicationController
   def show
     @song = Song.find_by(id: params[:id])
     if !@song
-      flash[:alert] = 'Song not Found'
+      flash[:alert] = 'Song not Found.'
       redirect_to artist_songs_path(params[:artist_id])
     end
   end
