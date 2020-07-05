6d817d073b6a644ca614b4ce46d8c6d64e348e84
Xiaoyi Liu <lxy878@gmail.com>
2020-07-05 16:01:23 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,7 +4,10 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    @artist = Artist.find_by(id:params[:id])
+    @artist = Artist.find_by(id: params[:id])
+    if !@artist
+      redirect_to artists_path
+    end
   end
 
   def new
